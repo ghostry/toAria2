26153ceee0748aa05b5ba970f8f939628b5b5545
ghostry <ghostry@ghostry.cn>
2017-06-09 23:03:12 +0800

    add 右键菜单

--- src/add.js
+++ src/add.js
@@ -2,56 +2,85 @@ chrome.downloads.onDeterminingFilename.addListener(add);
 var size = localStorage["size"] * 1024 * 1024;
 var path = localStorage["path"];
 function add(down) {
-	console.debug(down);
-	if (!path || !size) {
-		alert("插件尚未配置");
-		chrome.tabs.create({ "url": "options.html" }, function (s) { });
-		return 0;
-	}
-	if (Math.abs(down.fileSize) > size) {
-		this.aria2_obj = combination(down);
-		var ifpostback = postaria2obj(this.aria2_obj);
-		if (ifpostback == "base64_error") {
-			var notification = new Notification("成功！", { body: "添加任务至 aria2 出错！" });
-		} else {
-			chrome.downloads.cancel(down.id, function (s) { });
-			var notification = new Notification("成功！", { body: "下载已送往aria2，请前往确认" });
-		}
-	}
-	//alert(down.fileSize);
+    console.debug(down);
+    if (!path || !size) {
+        alert("插件尚未配置");
+        chrome.tabs.create({"url": "options.html"}, function (s) { });
+        return 0;
+    }
+    if (Math.abs(down.fileSize) > size) {
+        this.aria2_obj = combination(down);
+        var ifpostback = postaria2obj(this.aria2_obj);
+        if (ifpostback == "base64_error") {
+            var notification = new Notification("成功！", {body: "添加任务至 aria2 出错！"});
+        } else {
+            chrome.downloads.cancel(down.id, function (s) { });
+            var notification = new Notification("成功！", {body: "下载已送往aria2，请前往确认"});
+        }
+    }
 }
 function postaria2obj(addobj) {
-	var httppost = new XMLHttpRequest();
-	this.aria2jsonrpcpath = path;
-	httppost.open("POST", this.aria2jsonrpcpath + "?tm=" + (new Date()).getTime().toString(), true);
-	var ifregurl = aria2url_reg(this.aria2jsonrpcpath);
-	if (ifregurl) {
-		if (!window.btoa) {
-			return "base64_error";
-		} else {
-			httppost.setRequestHeader("Authorization", "Basic " + btoa(ifregurl));
-		}
-	}
-	httppost.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-	httppost.send(JSON.stringify(addobj));
-	return "ok";
+    var httppost = new XMLHttpRequest();
+    this.aria2jsonrpcpath = path;
+    httppost.open("POST", this.aria2jsonrpcpath + "?tm=" + (new Date()).getTime().toString(), true);
+    var ifregurl = aria2url_reg(this.aria2jsonrpcpath);
+    if (ifregurl) {
+        if (!window.btoa) {
+            return "base64_error";
+        } else {
+            httppost.setRequestHeader("Authorization", "Basic " + btoa(ifregurl));
+        }
+    }
+    httppost.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+    httppost.send(JSON.stringify(addobj));
+    return "ok";
 
 }
 function aria2url_reg(url) {
-	if (url.split("@")[0] == url) {
-		return null;
-	}
-	return url.split("@")[0].match("/^(http:\\/\\/\|https:\\/\\/)?(.*)\/")[2];
+    if (url.split("@")[0] == url) {
+        return null;
+    }
+    return url.split("@")[0].match("/^(http:\\/\\/\|https:\\/\\/)?(.*)\/")[2];
 }
 function combination(down) {
-	var post_obj = [{
-		"jsonrpc": "2.0",
-		"method": "aria2.addUri",
-		"id": (new Date()).getTime().toString(),
-		"params": [[down.finalUrl], {
-			"out": decodeURIComponent(down.filename),
-			"header":"Referer: " + down.referrer
-		}]
-	}];
-	return post_obj;
+    if (down.filename == '') {
+        var post_obj = [{
+                "jsonrpc": "2.0",
+                "method": "aria2.addUri",
+                "id": (new Date()).getTime().toString(),
+                "params": [[down.finalUrl], {
+                        "header": "Referer: " + down.referrer
+                    }]
+            }];
+    } else {
+        var post_obj = [{
+                "jsonrpc": "2.0",
+                "method": "aria2.addUri",
+                "id": (new Date()).getTime().toString(),
+                "params": [[down.finalUrl], {
+                        "out": decodeURIComponent(down.filename),
+                        "header": "Referer: " + down.referrer
+                    }]
+            }];
+    }
+    return post_obj;
 }
+
+function rightadd(info, tab) {
+    var down = {filename: ''};
+    down.finalUrl = info.linkUrl;
+    down.referrer = info.pageUrl;
+    if (!path || !size) {
+        alert("插件尚未配置");
+        chrome.tabs.create({"url": "options.html"}, function (s) { });
+        return 0;
+    }
+    this.aria2_obj = combination(down);
+    var ifpostback = postaria2obj(this.aria2_obj);
+    if (ifpostback == "base64_error") {
+        var notification = new Notification("成功！", {body: "添加任务至 aria2 出错！"});
+    } else {
+        var notification = new Notification("成功！", {body: "下载已送往aria2，请前往确认"});
+    }
+}
+chrome.contextMenus.create({"title": "aria2", "contexts": ["link"], "onclick": rightadd});
